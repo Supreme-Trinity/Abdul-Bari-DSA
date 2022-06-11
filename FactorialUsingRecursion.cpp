f9a0ae2310baa2f98078793ba59ff6629ccc0997
Supreme-Trinity <95674894+Supreme-Trinity@users.noreply.github.com>
2022-06-11 19:33:38 +0530

    Update FactorialUsingRecursion.cpp

--- FactorialUsingRecursion.cpp
+++ FactorialUsingRecursion.cpp
@@ -1,19 +1,28 @@
-
-#include <iostream>
-
+//Recursive Binary Search
+#include <bits/stdc++.h>
 using namespace std;
 
-int factorialusingRecursion(int n){
-    if(n>1){
-        return factorialusingRecursion(n-1)*n;
+int RbinarySearch(int arr[],int s,int e,int key){
+    if(s<=e){
+        int mid= floor((s+e)/2);
+        
+        if(arr[mid]==key){
+            return mid;
+        }
+        else if(key>arr[mid]){
+            RbinarySearch(mid+1,e,key);
+        }
+        else{
+            RbinarySearch(s,mid-1,key);
+        }
     }
-    return 1;
+    return -1;
 }
-
 int main()
-{   int x;
-    cin>>x;
-    int fac= factorialusingRecursion(x);
-    std::cout << fac << std::endl;
+{   int arr[5]={1,12,1212,121212,2323};
+    int index= RbinarySearch(arr,0,arr.length-1,2323);
+    std::cout << index << std::endl;
+   
+
     return 0;
 }
