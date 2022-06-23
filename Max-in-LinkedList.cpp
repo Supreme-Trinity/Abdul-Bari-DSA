642186247dc39c7330588cb0fec1d1f0c5016f1a
Supreme-Trinity <95674894+Supreme-Trinity@users.noreply.github.com>
2022-06-23 15:50:07 +0530

    Update Max-in-LinkedList.cpp

--- Max-in-LinkedList.cpp
+++ Max-in-LinkedList.cpp
@@ -9,3 +9,19 @@ int maxinLinkedlist(struct node *p){
     }
     return max;
 }
+
+
+//Now finding the same using recursion be like
+int recursivemax(struct node *p){
+    int x=0;
+    if(p==0){
+        return INT32_MIN;
+    }
+    x=recursivemax(p->next);
+    if(x>(p->data)){
+        return x;
+    }
+    else{
+        return p->data;
+    }
+}
